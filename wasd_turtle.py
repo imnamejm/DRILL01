dd7996a0745761e20929fa0bdfadd91cbab15ef3
imnamejm(Lect) <eileen031029@naver.com>
2023-09-15 22:11:09 +0900

    turtle_onkey
    키 입력 받게 만들기

---
+++ wasd_turtle.py
@@ -0,0 +1,33 @@
+import turtle
+
+turtle.shape('turtle')
+
+def w_move():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def a_move():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+
+def s_move():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+
+def d_move():
+    turtle.setheading(360)
+    turtle.forward(50)
+    turtle.stamp()
+
+def restart():
+    turtle.reset()
+
+turtle.onkey(w_move, 'w')
+turtle.onkey(a_move, 'a')
+turtle.onkey(s_move, 's')
+turtle.onkey(d_move, 'd')
+turtle.onkey(restart,'Escape')
+turtle.listen()
